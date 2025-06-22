d3305735d47aed03cc3a90e6ce45ff4108073fd2
amal123618 <amaljithkuvakkatt@gmail.com>
2025-06-22 23:05:51 +0530

    Update login.jsx

--- bloging/src/components/login.jsx
+++ bloging/src/components/login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://127.0.0.1:8000/login/", {
+      const response = await axios.post("https://blog-10-nrph.onrender.com//login/", {
         username,
         password
       });
